2cdef741f7e8928d4500b55bd6bca0cdfcea2d13
Caamano-01 <isabelecaamano87@gmail.com>
2026-03-16 14:07:24 -0300

    Tarefas

---
+++ src/components/Tarefa.jsx
@@ -0,0 +1,42 @@
+function Tarefa({ tarefa, excluir, alternar, editar }) {
+
+  const [editando, setEditando] = useState(false);
+  const [novoTexto, setNovoTexto] = useState(tarefa.texto);
+
+  const salvar = () => {
+    editar(tarefa.id, novoTexto);
+    setEditando(false);
+  };
+
+  return (
+    <div
+      className={`tarefa-card ${tarefa.completada ? 'concluida' : ''}`}
+      style={{ borderLeft: `8px solid ${tarefa.cor}` }}
+    >
+
+      {editando ? (
+        <input
+          value={novoTexto}
+          onChange={(e) => setNovoTexto(e.target.value)}
+          onBlur={salvar}
+          autoFocus
+        />
+      ) : (
+        <div 
+          className="tarefa-texto"
+          onClick={() => alternar(tarefa.id)}
+          onDoubleClick={() => setEditando(true)}
+        >
+          {tarefa.texto}
+        </div>
+      )}
+
+      <button className="btn-excluir" onClick={() => excluir(tarefa.id)}>
+        &times;
+      </button>
+
+    </div>
+  );
+}
+
+export default Tarefa;
